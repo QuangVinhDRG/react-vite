9949e2001657bd36139e2900e47589c5c06ddcc6
QuangVinh <bloodmoon.251103@gmail.com>
2024-06-25 11:04:34 +0700

    lesson 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,13 @@
+import "./style.scss";
 const MyComponent = () => {
   return (
-    <div>Eric & Hoi Dan IT</div>
+    <>
+      <div>Eric & Hoi Dan IT</div>
+      <div className="parent">
+        Parent
+        <div className="child">Child</div>
+      </div>
+    </>
   );
 }
 export default MyComponent;
